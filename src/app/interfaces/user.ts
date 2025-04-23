73c420e4710d1fc0929598ff6ae0bd2847ccd3cf
Alejandro <alexxsanse@gmail.com>
2025-04-23 11:54:17 +0200

    Se ha creado el servicio para registrar usuarios y la interfaz de los usuarios

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id?: number;
+    name: string;
+    age: number;
+    weight: number;
+    height: number;
+    gender: string;
+    activity_level: string;
+    goal: string;
+    daily_calories: number;
+  }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://ruizgijon.ddns.net/sancheza/api/evagym'; 
+
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User): Observable<any> {
+    console.log("Enviando usuario:", user);
+    return this.http.post(`${this.apiUrl}/create.php`, user, httpOptions);
+  }
+}
