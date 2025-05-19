e6678389bbd9693ae9bf8b558ecdd56665afedbf
Alejandro <alexxsanse@gmail.com>
2025-05-19 09:54:57 +0200

    v1.6.1 - Combinacion de controladores de usuarios

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,37 +5,66 @@ import { User } from '../interfaces/user';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
+    'Content-Type': 'application/json',
+  }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'https://ruizgijon.ddns.net/sancheza/evagym'; 
+  private apiUrl = 'https://ruizgijon.ddns.net/sancheza/evagym';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   createUser(user: User): Observable<any> {
-    console.log("Enviando usuario:", user);
-    return this.http.post(`${this.apiUrl}/controller/create.php`, user, httpOptions);
+    console.log('Enviando usuario:', user);
+    return this.http.post(
+      `${this.apiUrl}/controller/create.php`,
+      user,
+      httpOptions
+    );
   }
 
   login(credentials: { name: string; passwd: string }): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/controller/login.php`, credentials);
+    return this.http.post<any>(
+      `${this.apiUrl}/controller/login.php`,
+      credentials
+    );
   }
 
   getUserById(id: number): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/controller/getUser.php`, { id }, httpOptions);
+    return this.http.post<User>(
+      `${this.apiUrl}/controller/getUser.php`,
+      { id },
+      httpOptions
+    );
   }
 
+  // deleteUser(id: number): Observable<any> {
+  //   return this.http.post<any>(
+  //     `${this.apiUrl}/controller/deleteUser.php`,
+  //     { id },
+  //     httpOptions
+  //   );
+  // }
+
   deleteUser(id: number): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/controller/deleteUser.php`, { id }, httpOptions);
+    return this.http.post(`${this.apiUrl}/controller/user_controller.php`, {
+      action: 'deleteUser',
+      id: id,
+    });
   }
-  
+
+  // updateUser(user: User): Observable<any> {
+  //   console.log("Enviando actualización de usuario:", user);
+  //   return this.http.post<any>(`${this.apiUrl}/controller/updateUser.php`, user, httpOptions);
+  // }
+
   updateUser(user: User): Observable<any> {
-    console.log("Enviando actualización de usuario:", user);
-    return this.http.post<any>(`${this.apiUrl}/controller/updateUser.php`, user, httpOptions);
-  }  
+    return this.http.post(`${this.apiUrl}/controller/user_controller.php`, {
+      action: 'updateUser',
+      ...user,
+    });
+  }
 }
